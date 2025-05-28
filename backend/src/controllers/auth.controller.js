336109701140a379829691a3c12d45716d3b3f49
naina-chauhan <Nainachauhan027@gmail.com>
2025-05-28 17:50:39 +0530

    Initial commit

---
+++ backend/src/controllers/auth.controller.js
@@ -0,0 +1,14 @@
+export async function signup(req, res) {
+    // Handle signup logic here
+    res.send("Signup route");
+}
+
+export async function login(req, res) {
+    // Handle signup logic here
+    res.send("Signup route");
+}
+
+export function logout(req, res) {
+    // Handle signup logic here
+    res.send("Signup route");
+}
