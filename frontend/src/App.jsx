b65dfaf2e2f47bd7c7069b44cba17ee594e8f255
ghanshyam-du <ghanshyamd816@gmail.com>
2025-07-24 18:10:00 +0530

    Frontend updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx"
@@ -6,6 +7,9 @@ import NotificationsPage from "./pages/NotificationsPage.jsx"
 import CallPage from "./pages/CallPage.jsx";
 import ChatPage from "./pages/ChatPage.jsx";
 import OnboardingPage from "./pages/OnboardingPage.jsx";
+
+import { Toaster } from 'react-hot-toast';
+
 const App = () => {
   return (
     <div className="h-screen" data-theme="forest">
