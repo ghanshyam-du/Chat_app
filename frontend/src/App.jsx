7944f517d2dc66ac389ce7480f5f4094045ba9e9
ghanshyam-du <ghanshyamd816@gmail.com>
2025-08-11 21:56:28 +0530

    Signup page update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 import HomePage from "./pages/HomePage.jsx";
 import SignUpPage from "./pages/SignUpPage.jsx";
 import LoginPage from "./pages/LoginPage.jsx"
@@ -16,30 +16,30 @@ import { axiosInstance } from './lib/axios.js';
 const App = () => {
 
   //tanstack query
-  const {data, isLoading, error} = useQuery({
-    queryKey: ["todos"],
+  const {data: authData, isLoading, error} = useQuery({
+    queryKey: ["authUser"],
     queryFn: async() =>{
       const res = await axiosInstance.get("/auth/me");
       return res.data;
     },
     retry: false,
   })
-  console.log(data);
+  
+  const authUser = authData?.user 
+
     
-console.log({data});
-console.log({isLoading});
-console.log({error});
+
   return (
     <div className="h-screen" data-theme="forest">
       
       <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/signup" element={<SignUpPage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="/notifications" element={<NotificationsPage />} />
-        <Route path="/call" element={<CallPage />} />
-        <Route path="/chat" element={<ChatPage />} />
-        <Route path="/onboarding" element={<OnboardingPage />} />
+        <Route path="/" element={authUser ? <HomePage /> : <Navigate to = "/login" />} />
+        <Route path="/signup" element={!authUser ? <SignUpPage /> : <Navigate to ="/" />} />
+        <Route path="/login" element={ !authUser ? <LoginPage /> : <Navigate to ="/" />} />
+        <Route path="/notifications" element={authUser ? <NotificationsPage /> : <Navigate to = "/login" />} />
+        <Route path="/call" element={authUser ? <CallPage /> : <Navigate to = "/login" /> } />
+        <Route path="/chat" element={authUser ? <ChatPage /> : <Navigate to = "/login" />} />
+        <Route path="/onboarding" element={authUser ? <OnboardingPage /> : <Navigate to = "/login" />} />
 
       </Routes>
       
--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,9 +1,26 @@
 import React from 'react'
+import { useState } from 'react'
 
 const SignUpPage = () => {
+  const [singupData, setSignupData] = useState({
+    fullName: "",
+    email: "",
+    password: "",
+  })
+
+  const handleSignup = (e) =>{
+    e.preventDefault()
+  }
+
   return (
-    <div>
-      
+    <div className = "h-screen flex items-center justify-center p-4 sm:p-6 md:p-8" data-theme="forest">
+      <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-xl overflow-hidden">
+         {/* signup form - left side*/}
+
+         <div  >
+
+         </div>
+      </div>
     </div>
   )
 }
