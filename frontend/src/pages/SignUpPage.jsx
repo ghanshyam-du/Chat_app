65101ceaffae359811a78485a1379986e8b0b511
ghanshyam-du <ghanshyamd816@gmail.com>
2025-08-15 23:49:39 +0530

    Signup Page updated

--- frontend/src/pages/SignUpPage.jsx
+++ frontend/src/pages/SignUpPage.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { useState } from 'react'
+import { ShipWheelIcon } from "lucide-react"; 
 
 const SignUpPage = () => {
   const [singupData, setSignupData] = useState({
@@ -17,7 +18,29 @@ const SignUpPage = () => {
       <div className="border border-primary/25 flex flex-col lg:flex-row w-full max-w-5xl mx-auto bg-base-100 rounded-xl shadow-xl overflow-hidden">
          {/* signup form - left side*/}
 
-         <div  >
+         <div  className="w-full lg:w-1/2 p-4 sm:p-8 flex flex-col">
+         {/* LOGO*/}
+         <div className="mb-4 flex items-center justify-start gap-2">
+          <ShipWheelIcon className="size-9 text-primary" />
+          <span className='text-3xl font-bold font-mono bg-clip-text text-transparent bg-gradient-to-r from-primary to-secondary tracking-wider'>
+            Chit-Chat
+          </span>
+
+
+         </div>
+         <div className='w-full'>
+          <form onSubmit={handleSignup}>
+            <div className='space-y-4'>
+              <div>
+                <h2 className='text-xl font-semibold'>Create an Account</h2>
+                <p className='text-sm opacity-70'>
+                  Join Chit-Chat and Connect with you Friends and Family!
+                </p>
+              </div>
+            </div>
+          </form>
+
+         </div>
 
          </div>
       </div>
