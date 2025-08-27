d35f9f93462582ea87b3647773290e24d029c7b4
ghanshyam-du <ghanshyamd816@gmail.com>
2025-08-27 09:47:58 +0530

    Onboarding updated

--- frontend/src/lib/api.js
+++ frontend/src/lib/api.js
@@ -3,3 +3,15 @@ export const signup = async(singupData) =>{
       const response = await axiosInstance.post("/auth/signup", singupData);
       return response.data;
 }
+
+export const getAuthUser = async () => {
+      const res = await axiosInstance.get("/auth/me");
+      return res.data;
+}
+
+
+export const completeOnboarding = async()=>{
+      const response = await axiosInstance.post("/auth/onboarding",userData);
+      return response.data;
+
+}
--- frontend/src/pages/OnboardingPage.jsx
+++ frontend/src/pages/OnboardingPage.jsx
@@ -1,8 +1,66 @@
-import React from 'react'
+import { useState } from "react";
+import useAuthUser from "../hooks/useAuthUser"
+import { QueryClient, useMutation, useQueryClient } from "@tanstack/react-query";
+import toast from "react-hot-toast";
+import {completeOnboarding} from "../lib/api"
+import { CameraIcon } from "lucide-react";
+
 
 const OnboardingPage = () => {
+
+  const {authUser} = useAuthUser();
+  const queryClient = useQueryClient();
+  const[formState,setFromState] = useState({
+    fullName: authUser?.fullName || "",
+    bio: authUser?.bio ||"",
+    nativeLanguage: authUser?.nativeLanguage ||"",
+    learningLanguage: authUser?.learningLanguage ||"",
+    location: authUser?.location ||"",
+    profilePic: authUser?.profilePic ||"",
+
+  });
+
+  const {mutate: onboardingMutation, isPending}=useMutation({
+    mutationFn: completeOnboarding,
+    onSuccess:()=>{
+      toast.success("Profile onboarded successfully");
+      QueryClient.invalidateQueries({queryKey: ["authUser"]});
+    }
+  })
+
+  const handleSubmit = (e) =>{
+    e.preventDefault();
+
+    onboardingMutation(formState);
+  }
+  
+
+
   return (
-    <div>
+    <div className="min-h-screen bg-base-100 flex items-center justify-center p-4">
+      <div>
+        <div>
+          <h1 className="text-2xl sm:text-3xl font-bold text-center mb-6">
+            Complete Your Profile
+          </h1>
+        </div>
+        <form onSubmit={handleSubmit} className="space-y-6">
+          <div className="flex flex-col items-center justify-center space-y-4">
+            <div className="size-32 rounded-full bg-base-300 overflow-hidden">
+              {
+                formState.profilePic ? (
+                  <img src={formState.profilePic} alt="profile preview" className="w-full h-full object-cover" />
+                ):(
+                  <div className="flex items-center justify-center h-full">
+                    <CameraIcon className="size-12 text-base-content opacity-40" />
+                  </div>
+                )
+              }
+            </div>
+
+          </div>
+        </form>
+      </div>
       
     </div>
   )
