5d22c19979d74a7322b990698bc19a0b61af0dbb
ghanshyam-du <ghanshyamd816@gmail.com>
2025-08-30 21:48:56 +0530

    onboarding updated

--- frontend/src/hooks/useAuthUser.js
+++ frontend/src/hooks/useAuthUser.js
@@ -1,18 +1,6 @@
 import { useQuery } from "@tanstack/react-query"
 import { getAuthUser } from "../lib/api"
 
-// const useAuthUser = () =>{
-//      const authUser = useQuery({
-//     queryKey: ["authUser"],
-//     queryFn: getAuthUser ,
-//     retry: false,
-//   })
-
-//   return {isLoading: authUser.isLoading, authUser: authUser.data?.user}
-// };
-
-
-
 
 const useAuthUser = () => {
   const authUser = useQuery({
